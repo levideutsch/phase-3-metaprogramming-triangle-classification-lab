d0cd4313ce5a959c6b7300bafa463bdf9a462f44
levideutsch <levideutsch@live.com>
2023-03-14 14:05:12 -0400

    commit tree 50da4cfc2bcd3b531d7ec5f2a14bd212d01f8cd6

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+   @a = a
+   @b = b
+   @c = c 
+  end
+
+  def kind
+  if  @a <= 0 || @b <= 0 || @c <= 0 
+    raise TriangleError
+  elsif
+    @a + @b < @c || @b + @c < @a || @a + @c <= @b
+    raise TriangleError
+  elsif
+    @a == @b && @a == @c && @b == @c 
+    :equilateral
+  elsif @a == @b || @a == @c || @b == @c 
+    :isosceles
+  elsif @a != @b && @a != @c && @b != @c 
+    :scalene
+  end
 end
+
+  class TriangleError < StandardError
+    # triangle error code
+end
+  
+end
+
